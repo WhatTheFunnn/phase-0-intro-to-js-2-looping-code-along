cc3322a61a55dc34b7bdec4b952d0fa0de900552
Dan Souza <Souza.music@outlook.com>
2022-01-20 22:06:19 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ("Guadalupe", "Ollie", "Aki");
+
+function writeCards(names) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+    cards[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+        return cards;
+}
+
+function countDown (countdown) {
+    for (let countdown = 10; countdown >= 0; countdown--) {
+        console.log(countdown);
+    }
+}
+    
